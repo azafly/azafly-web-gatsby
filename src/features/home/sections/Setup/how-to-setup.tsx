becbbcb33e4d5fe109dafa263b0601eb36a745af
Ola John Ajiboye <33224920+olajohn-ajiboye@users.noreply.github.com>
2021-09-21 19:41:03 +0200

    home UI fix

--- src/features/home/sections/Setup/how-to-setup.tsx
+++ src/features/home/sections/Setup/how-to-setup.tsx
@@ -2,51 +2,48 @@ import React from "react";
 import { Grid, makeStyles, Theme, createStyles } from "@material-ui/core";
 import { Illustration } from "./illustration";
 import { InfoBanner } from "./info-banner";
-import { HomeImages } from '../../../../createPages/templates/home'
+import { HomeImages } from "../../../../createPages/templates/home";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        container: {
-            flexGrow: 1,
-            width: "80%",
-            margin: "auto",
-            height: 800,
-            paddingTop: 20,
-            display: "flex",
-            [theme.breakpoints.only("xs")]: {
-                marginTop: 10,
-                padding: "10px 10px",
-                width: "100%",
-                margin: "auto",
-                marginBottom: 80,
-            },
-            [theme.breakpoints.only("md")]: {
-                width: "80%",
-            },
-            [theme.breakpoints.only("sm")]: {
-                width: "85%",
-            },
-        },
-    })
+  createStyles({
+    container: {
+      flexGrow: 1,
+      width: "80%",
+      margin: "auto",
+      display: "flex",
+      [theme.breakpoints.only("xs")]: {
+        padding: "10px 10px",
+        width: "100%",
+        margin: "auto",
+        marginBottom: 80,
+      },
+      [theme.breakpoints.only("md")]: {
+        width: "80%",
+      },
+      [theme.breakpoints.only("sm")]: {
+        width: "85%",
+      },
+    },
+  })
 );
 
 interface HomePageHeaderSectionProps {
-    isMobileScreenSize?: boolean;
-    images: HomeImages;
+  isMobileScreenSize?: boolean;
+  images: HomeImages;
 }
 
 export const HowToSetUp = ({ images }: HomePageHeaderSectionProps) => {
-    const classes = useStyles();
-    return (
-        <div className={classes.container}>
-            <Grid container justify="space-around" alignItems="center">
-                <Grid item xs={12} sm={6}>
-                    <Illustration images={images} />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <InfoBanner />
-                </Grid>
-            </Grid>
-        </div>
-    );
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <Grid container justify="space-around" alignItems="center">
+        <Grid item xs={12} sm={6}>
+          <Illustration images={images} />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <InfoBanner />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
--- src/features/home/sections/Setup/info-banner.tsx
+++ src/features/home/sections/Setup/info-banner.tsx
@@ -1,5 +1,5 @@
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import { Button, Typography } from "@material-ui/core";
+import { Box, Button, Typography } from "@material-ui/core";
 import { motion, Variants } from "framer-motion";
 import VerifiedUser from "@material-ui/icons/VerifiedUser";
 import { Link } from "gatsby-theme-material-ui";
@@ -8,75 +8,32 @@ import React from "react";
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
-      boxShadow: "0 2px 16px 0 rgb(0 0 0 / 8%);",
-      borderRadius: 25,
-      padding: 150,
-      [theme.breakpoints.up("sm")]: {
-        height: "80%",
-        marginLeft: "-25%",
-      },
+      margin: "auto",
       [theme.breakpoints.only("xs")]: {
-        boxShadow: "none",
-        padding: 0,
-        margin: 0,
+        textAlign: "center",
+        paddingTop: 50,
       },
-      "& .link": {
-        textDecoration: "none",
+      [theme.breakpoints.up("sm")]: {
+        paddingLeft: 50,
       },
     },
     titleHeading: {
       color: theme.colors.black,
       fontFamily: "Nunito",
       fontWeight: 900,
-      fontSize: 40,
-      marginBottom: 30,
-      marginLeft: "25%",
-      [theme.breakpoints.down("md")]: {
-        fontSize: 35,
-      },
-      [theme.breakpoints.only("xs")]: {
-        textAlign: "center",
-        paddingTop: 50,
-        marginLeft: 0,
-      },
+      fontSize: 30,
+      marginBottom: 20,
     },
     actionButton: {
       background: theme.colors.mainGreen,
       textTransform: "none",
-      marginLeft: "25%",
       color: "white",
       marginTop: 20,
       fontWeight: 450,
-      width: 170,
       padding: "7px 25px",
       "&:hover": {
         background: "#4990a4",
         opacity: 0.7,
-      },
-      [theme.breakpoints.only("xs")]: {
-        marginLeft: "0%",
-      },
-    },
-    items: {
-      marginLeft: "25%",
-    },
-    item: {
-      display: "flex",
-      marginLeft: 10,
-      margin: "auto",
-      [theme.breakpoints.only("xs")]: {
-        marginLeft: "0%",
-      },
-      "& span": {
-        color: theme.colors.black,
-        fontSize: "0.9rem",
-        fontFamily: "Nunito",
-        fontWeight: 700,
-        marginBottom: 20,
-        marginLeft: 20,
-      },
-      "& .MuiSvgIcon-root": {
-        color: theme.colors.mainGreen,
       },
     },
   })
@@ -86,73 +43,26 @@ interface InfoBannerProps {
   isMobileScreenSize?: boolean;
 }
 
-const containerVariants: Variants = {
-  enter: {
-    x: 0,
-    opacity: 1,
-    decelerate: 20,
-    transition: {
-      when: "beforeChildren",
-      staggerChildren: 0.4,
-    },
-  },
-  exit: { x: -300, opacity: 0 },
-};
-
-const childVariants: Variants = {
-  enter: {
-    x: 0,
-    opacity: 1,
-  },
-  exit: {
-    opacity: 0,
-  },
-};
-
-const childVariants2: Variants = {
-  enter: {
-    x: 0,
-    opacity: 1,
-  },
-  exit: {
-    opacity: 0,
-  },
-};
-
 export const InfoBanner = () => {
   const classes = useStyles();
 
   return (
-    <motion.div className={classes.container}>
-      <Typography variant="h4" className={classes.titleHeading}>
-        Set up in 3 steps{" "}
-      </Typography>
-      <motion.ul
-        layout
-        variants={containerVariants}
-        className={classes.items}
-        initial="exit"
-        animate="enter"
-        exit="exit"
-      >
-        <motion.li variants={childVariants} className={classes.item}>
-          {" "}
-          <VerifiedUser /> <span> Open a Free Account</span>{" "}
-        </motion.li>
-        <motion.li variants={childVariants} className={classes.item}>
-          {" "}
-          <VerifiedUser /> <span>Make a Request</span>{" "}
-        </motion.li>
-        <motion.li variants={childVariants2} className={classes.item}>
-          <VerifiedUser /> <span>Done</span>{" "}
-        </motion.li>
-      </motion.ul>
-
-      <div>
-        <Link className="link" to="/signup">
-          {" "}
-          <Button className={classes.actionButton}>Get started</Button>
-        </Link>
+    <motion.div>
+      <div className={classes.container}>
+        <Typography variant="h4" className={classes.titleHeading} gutterBottom>
+          A better way to plan
+        </Typography>
+        <Typography paragraph>
+          Live your life on your terms and without limits. With an Azafly
+          account, you can add money to your wallet for free, within seconds and
+          start making transfers to other wallets or any bank accounts for
+          free.*
+          <br />
+          <Link className="link" to="/signup">
+            {" "}
+            <Button className={classes.actionButton}>Get started</Button>
+          </Link>
+        </Typography>
       </div>
     </motion.div>
   );
