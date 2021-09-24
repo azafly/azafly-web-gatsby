f7c85aec8001c34d59b62ce3f9abaef96e2e556c
Ola John Ajiboye <33224920+olajohn-ajiboye@users.noreply.github.com>
2021-09-24 10:38:21 +0200

    update home seo description

--- src/createPages/templates/home.tsx
+++ src/createPages/templates/home.tsx
@@ -4,58 +4,53 @@ import { graphql } from "gatsby";
 import { Layout } from "../../components/layout";
 import { SEO } from "../../components/seo";
 
-import HomeView from '../../features/home';
-import { FluidImageType } from '../../types'
-
-
-
-
-
+import HomeView from "../../features/home";
+import { FluidImageType } from "../../types";
 
 export const pageQuery = graphql`
- query HomePageImages{
+  query HomePageImages {
     heroMainImage1: file(relativePath: { eq: "images/home-main.jpeg" }) {
       childImageSharp {
         # Specify the image processing specifications right in the query.
         # Makes it trivial to update as your page's design changes.
-        fluid(maxWidth: 800,maxHeight:600, quality: 90) {
-                  ...GatsbyImageSharpFluid_withWebp
+        fluid(maxWidth: 800, maxHeight: 600, quality: 90) {
+          ...GatsbyImageSharpFluid_withWebp
         }
       }
     }
-     setupImage: file(relativePath: { eq: "images/home-setup.jpeg"}) {
+    setupImage: file(relativePath: { eq: "images/home-setup.jpeg" }) {
       childImageSharp {
         # Specify the image processing specifications right in the query.
         # Makes it trivial to update as your page's design changes.
-         fluid(maxWidth: 800,maxHeight:600, quality: 90) {
-                  ...GatsbyImageSharpFluid_withWebp
+        fluid(maxWidth: 800, maxHeight: 600, quality: 90) {
+          ...GatsbyImageSharpFluid_withWebp
         }
       }
-    },
-}
-`
-
+    }
+  }
+`;
 
 export interface HomeImages {
-    heroMainImage1: FluidImageType,
-    setupImage: FluidImageType
+  heroMainImage1: FluidImageType;
+  setupImage: FluidImageType;
 }
 
-
 interface Home {
-    data: HomeImages
+  data: HomeImages;
 }
 
 const Home: FunctionComponent<Home> = ({ data }) => {
-
-    return (
-        <>
-            <SEO title="Home" image="/logo.png" />
-            <Layout>
-                <HomeView images={data} />
-            </Layout>
-        </>
-    );
+  return (
+    <>
+      <SEO
+        title=" | Emmigrate and Make Complex International Payments Easily"
+        image="/logo.png"
+      />
+      <Layout>
+        <HomeView images={data} />
+      </Layout>
+    </>
+  );
 };
 
 export default Home;
