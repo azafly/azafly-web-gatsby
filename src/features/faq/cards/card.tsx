2219136f3fcc1f3b204c3561f90f0863401b2ff7
Ola John Ajiboye <33224920+olajohn-ajiboye@users.noreply.github.com>
2021-09-24 17:03:56 +0200

    add footer config

--- src/features/faq/cards/card.tsx
+++ src/features/faq/cards/card.tsx
@@ -1,37 +1,39 @@
-import React from 'react';
-import { Box, Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from '@material-ui/core';
+import React from "react";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 
-
-import { useCardStyles } from '../classes';
-
+import { useCardStyles } from "../classes";
 
 export function Cards() {
-    const classes = useCardStyles();
+  const classes = useCardStyles();
 
-    return (
-        <Grid item xs={12} sm={6} md={3}>
-            <Box boxShadow={1}>
-                <Card className={classes.root} elevation={0}>
-                    <CardActionArea className={classes.action} >
-                        <CardMedia
-                            className={classes.media}
-                            image="https://www.ntb.de/wp-content/uploads/2019/11/placeholder-image-icon-21.jpg"
-                            title="Contemplative Reptile"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2" align="center">
-                                How to pay
-          </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                across all continents except Antarctica
-          </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-            </Box>
-
-        </Grid>
-
-    );
+  return (
+    <Grid item xs={12} sm={6} md={3}>
+      <Card className={classes.root} elevation={0}>
+        <CardActionArea className={classes.action}>
+          <CardMedia
+            className={classes.media}
+            image="https://www.ntb.de/wp-content/uploads/2019/11/placeholder-image-icon-21.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="h6" align="center">
+              How to pay
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
 }
