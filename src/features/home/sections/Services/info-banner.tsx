b58b353af7f9709ec1b4d18bed470e57c4897320
Adegboye <adedayo.adegboye@ger.win.int.kn>
2021-10-27 20:47:50 +0300

    services

--- src/features/home/sections/Services/info-banner.tsx
+++ src/features/home/sections/Services/info-banner.tsx
@@ -1,9 +1,7 @@
 import { motion } from 'framer-motion';
-import { makeStyles, createStyles, Theme, useTheme } from '@material-ui/core/styles';
-import { Button, Typography, Grid, Box, Link, Snackbar, InputAdornment, CircularProgress } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { Typography, Grid, Box, Card, CardContent, CardHeader, Avatar } from '@material-ui/core';
+import React from 'react';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -22,6 +20,29 @@ const useStyles = makeStyles((theme: Theme) =>
                 padding: '10px 30px 10px 20px',
                 marginLeft: '2vw',
                 width: '100%'
+            }
+        },
+
+        card: {
+            border: 'none',
+            boxShadow: 'none',
+            width: '100%',
+            marginTop: 20,
+            marginBottom: 20,
+            '&:hover': {
+                '& $circle': {
+                    color: 'white',
+                    background: '#0D324D'
+                },
+                '& $hr': {
+                    width: '200px',
+                    borderBottom: '4px solid #0D324D',
+                    opacity: '100%'
+                },
+
+                '& $heading': {
+                    fontWeight: 600
+                }
             }
         },
 
@@ -72,8 +93,6 @@ const useStyles = makeStyles((theme: Theme) =>
             }
         },
         heading: {
-            marginTop: 30,
-            marginBottom: 5,
             fontWeight: 400,
             fontSize: '26px',
             lineHeight: '32px',
@@ -81,72 +100,29 @@ const useStyles = makeStyles((theme: Theme) =>
             textAlign: 'left',
             verticalAlign: 'top',
             color: '#0D324D',
-            '&:hover': {
-                fontWeight: 600
-            },
+
             [theme.breakpoints.only('xs')]: {
                 textAlign: 'center !important',
                 fontSize: '20px'
             }
         },
-        customCard: {
-            display: 'flex',
-            border: 'none',
-            boxShadow: 'none',
-            [theme.breakpoints.only('xs')]: {
-                textAlign: 'center',
-                justifyItems: 'center',
-                justifyContent: 'center !important'
-            }
-        },
+
         circle: {
             background: 'transparent',
-            height: '46px',
-            width: '46px',
-            justifyContent: 'center !important',
             border: '1px solid #0D324D',
-            color: '#0D324D',
-            '&:hover': {
-                color: 'white',
-                background: '#0D324D'
-            }
+            color: '#0D324D'
         },
-        icon: {
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            [theme.breakpoints.only('xs')]: {
-                width: '100%',
-                justifyContent: 'center !important'
-            }
-        },
-        forward: {
-            justifyContent: 'flex-end',
-            [theme.breakpoints.only('xs')]: {
-                width: '100%',
-                justifyContent: 'center !important'
-            }
-        },
-        backward: {
-            justifyContent: 'flex-start',
-            [theme.breakpoints.only('xs')]: {
-                width: '100%',
-                justifyContent: 'center !important'
-            }
-        },
+
         hr: {
             width: '100%',
             height: '25px',
             borderBottom: '1px solid #0D324D',
-            transition: 'width 0.3s ease',
+            transition: 'width 0.5s ease',
             marginBottom: 10,
             justifyContent: 'center !important',
             opacity: '40%',
             position: 'relative',
-            '&:hover': {
-                width: '167px',
-                borderBottom: '4px solid #0D324D',
-                opacity: '100%'
-            },
+
             [theme.breakpoints.only('xs')]: {
                 width: '126px',
                 margin: 'auto',
@@ -160,7 +136,6 @@ const useStyles = makeStyles((theme: Theme) =>
 export const ServiceInfoBanner = () => {
     const classes = useStyles();
 
-    const theme = useTheme();
     const data = [
         { title: 'Lowest Fee', content: 'Our fee is the lowest in the market compare to any other', hoverWidth: '167px' },
         { title: ' Fast Transactions', content: 'The sky was cloudless and of a deep dark blue spectacle before.', hoverWidth: '267px' },
@@ -175,30 +150,31 @@ export const ServiceInfoBanner = () => {
                 {data.map((item, index) => {
                     return (
                         <Grid key={index} container spacing={2}>
-                            <Box clone order={{ xs: 1, sm: 1 }}>
-                                <Grid item xs={12} sm={9} className={classes.backward} container>
-                                    <Typography className={classes.heading}>{item.title}</Typography>
-                                </Grid>
-                            </Box>
-                            <Box clone order={{ xs: 4, sm: 1 }}>
-                                <Grid item xs={12} sm={2} className={classes.forward} container>
-                                    <Box className={classes.circle} sx={{ borderRadius: '50%' }}>
-                                        <KeyboardArrowRightIcon />
+                            <Card className={classes.card}>
+                                <CardHeader
+                                    action={
+                                        <Avatar className={classes.circle}>
+                                            <KeyboardArrowRightIcon />
+                                        </Avatar>
+                                    }
+                                    title={<Typography className={classes.heading}>{item.title}</Typography>}
+                                />
+                                <CardContent>
+                                    <Box clone order={{ xs: 2, sm: 2 }}>
+                                        <Grid item xs={12}>
+                                            <Typography variant='subtitle1' component='p' className={classes.content}>
+                                                {item.content}
+                                            </Typography>
+                                        </Grid>
                                     </Box>
-                                </Grid>
-                            </Box>
-                            <Box clone order={{ xs: 2, sm: 2 }}>
-                                <Grid item xs={12}>
-                                    <Typography variant='subtitle1' component='p' className={classes.content}>
-                                        {item.content}
-                                    </Typography>
-                                </Grid>
-                            </Box>
-                            <Box clone order={{ xs: 3, sm: 3 }}>
-                                <Grid item xs={12} sm={11} justifyContent='center'>
-                                    <Box className={classes.hr} />
-                                </Grid>
-                            </Box>
+
+                                    <Grid item xs={12} sm={11} justifyContent='center'>
+                                        <Box clone order={{ xs: 3, sm: 3 }}>
+                                            <Box className={classes.hr} />
+                                        </Box>
+                                    </Grid>
+                                </CardContent>
+                            </Card>
                         </Grid>
                     );
                 })}
