b9651a3dd2b3d8ffe1185e6a36fd6e85b7e0cd5a
Adegboye <adedayo.adegboye@ger.win.int.kn>
2021-11-01 13:04:56 +0200

    add stactic about_events pictures

--- src/features/about/sections/events-view/index.tsx
+++ src/features/about/sections/events-view/index.tsx
@@ -1,67 +1,16 @@
-import { makeStyles, Theme, createStyles, Box, Container, Grid, Paper, Card, CardMedia } from '@material-ui/core';
+import { makeStyles, Theme, createStyles, Box, Container, Grid } from '@material-ui/core';
 import React from 'react';
-import EventsTable from './event-table';
-import { SliderGallery } from './slider';
+import FlipCard from './flip-card';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        root: {
-            padding: 20
-        },
-        color: {
-            color: theme.colors.mainGreen
-        },
-        text: {
-            marginBottom: 50,
-            marginTop: 10,
-            flexGrow: 1,
-            marginTop: '-80px',
-            marginLeft: '10vw',
-            marginRight: '10vw',
-            [theme.breakpoints.only('md')]: {
-                marginLeft: '8vw'
-            },
-            [theme.breakpoints.only('sm')]: {
-                marginLeft: '7vw'
-            },
-            [theme.breakpoints.only('xs')]: {
-                margin: 'auto',
-                marginTop: '-50px',
-                marginBottom: 50
-            }
-        },
-        innerText: {
-            marginBottom: 50,
-            marginTop: 150,
-
-            marginLeft: '10vw',
-            marginRight: '10vw',
-            [theme.breakpoints.only('md')]: {
-                marginLeft: '8vw',
-                marginRight: '8vw'
-            },
-            [theme.breakpoints.only('sm')]: {
-                marginLeft: '7vw'
-            },
-            [theme.breakpoints.only('xs')]: {
-                margin: 'auto',
-                marginTop: 50,
-                marginBottom: 50
-            }
-        },
         title: {
             fontSize: 35,
             fontWeight: 'bold',
-            marginBottom: 20
-        },
-        subTitle: {
-            fontSize: 15
-        },
-        card: {
-            width: 345,
+            marginBottom: 20,
             [theme.breakpoints.only('xs')]: {
-                alignContent: 'center',
-                alignItems: 'center'
+                margin: 'auto',
+                textAlign: 'center'
             }
         }
     })
@@ -69,8 +18,9 @@ const useStyles = makeStyles((theme: Theme) =>
 
 interface Events {
     date: string;
-    name: string;
-    location: string;
+    image: string;
+    title: string;
+    content: string;
 }
 
 interface IEventsProps {
@@ -79,42 +29,21 @@ interface IEventsProps {
 
 const EventsView: React.FunctionComponent<IEventsProps> = ({ eventsData }) => {
     const classes = useStyles();
-    const alternatingColor = ['#f5f6f7', '#a9a9a9'];
 
     return (
         <>
             <Box>
                 <Container>
-                    <div className={classes.title}>All azafly events</div>
-                    <Grid
-                        container
-                        spacing={0}
-                        style={{
-                            alignContent: 'center',
-                            alignItems: 'center',
-                            width: '100%',
-                            marginBottom: 200
-                        }}
-                    >
-                        {eventsData?.map((event, index) => {
+                    <Box className={classes.title}>All azafly events</Box>
+                    <Grid container spacing={2}>
+                        {eventsData.map((listData, index) => {
                             return (
-                                <Grid key={index} style={{ bottom: 0 }} item xs={12} sm={12} md={12}>
-                                    <Box>
-                                        <EventsTable
-                                            color={alternatingColor[index % alternatingColor.length]}
-                                            name={event.name}
-                                            date={event.date}
-                                            location={event.location}
-                                        />{' '}
-                                    </Box>
-                                </Grid>
+                                <FlipCard key={index} image={listData.image} title={listData.title} content={listData.content} date={listData.date} />
                             );
                         })}
                     </Grid>
                 </Container>
             </Box>
-            <br />
-            <SliderGallery />
         </>
     );
 };
--- src/pages/about/events.tsx
+++ src/pages/about/events.tsx
@@ -4,10 +4,38 @@ import AboutTemplate from '../../features/about/about-template';
 
 const Events: React.FunctionComponent = () => {
     const eventsData = [
-        { date: '1 SEP', name: 'Getting over the top', location: 'Germany' },
-        { date: '1 SEP', name: 'Getting over the top', location: 'Germany' },
-        { date: '1 SEP', name: 'Getting over the top', location: 'Germany' },
-        { date: '1 SEP', name: 'Getting over the top', location: 'Germany' }
+        {
+            date: '1 Apr 2018',
+            title: 'CEO party',
+            image: 'https://images.unsplash.com/photo-1556125574-d7f27ec36a06?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
+            content: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+        },
+        {
+            date: '12 Oct 2020',
+            title: 'youth conference',
+            image: 'https://images.unsplash.com/photo-1523580494863-6f3031224c94?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
+            content:
+                ' Ullamcorper a lacus vestibulum sed arcu non odio euismod. Est velit egestas dui id ornare arcu odio ut. Integer feugiat scelerisque varius morbi enim nunc faucibus.'
+        },
+        {
+            date: '27 Sep 2020',
+            title: 'african gala',
+            image: 'https://images.unsplash.com/photo-1510743006598-4845616e044f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
+            content: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+            date: '3 May 2021',
+            title: 'Best initiative award',
+            image: 'https://images.unsplash.com/photo-1561912774-79769a0a0a7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80',
+            content:
+                ' Ullamcorper a lacus vestibulum sed arcu non odio euismod. Est velit egestas dui id ornare arcu odio ut. Integer feugiat scelerisque varius morbi enim nunc faucibus.'
+        },
+        {
+            date: '17 June 2021',
+            title: 'A new year',
+            image: 'https://images.unsplash.com/photo-1601601392622-5d18104a78fe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
+            content: ' A communit out react to give back to the children with special needs'
+        }
     ];
     return (
         <>
