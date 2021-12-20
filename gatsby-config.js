53e485d0db7ef38782888871586366c88df0b25c
Ola John Ajiboye <33224920+olajohn-ajiboye@users.noreply.github.com>
2021-12-20 11:48:38 +0100

    config yaml with dfferent image

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,9 +47,7 @@ module.exports = {
             options: {
                 // You can add multiple tracking ids and a pageview event will be fired for all of them.
                 trackingIds: [
-                    'GA-TRACKING_ID', // Google Analytics / GA
-                    'AW-CONVERSION_ID', // Google Ads / Adwords / AW
-                    'DC-FLOODIGHT_ID' // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+                    'UA-201238935-1' // Google Analytics / GA
                 ],
                 // This object gets passed directly to the gtag config command
                 // This config will be shared across all trackingIds
